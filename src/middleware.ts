0370544ac6fb144943ed10e5c4113eeda0865825
Ankijit2 <ankijit714@gmail.com>
2025-03-25 10:04:26 +0530

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,13 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
 
 // Define paths that require admin access
-const adminRoutes = ["/admin", "/api/admin"];
 
-export async function middleware(req: NextRequest) {
-    // Extract token from NextAuth session
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET, secureCookie: process.env.NODE_ENV === "production" });
 
-    // If no token, redirect to login pageF
-    if (!token) {
-        return NextResponse.redirect(new URL("/api/auth/signin", req.url));
+export async function middleware() {
+    if (process.env.NODE_ENV === 'production') {
+      return NextResponse.next();
     }
-
-    // Check if the route requires admin access
-    const isAdminRoute = adminRoutes.some((route) => req.nextUrl.pathname.startsWith(route));
-
-    if (isAdminRoute) {
-        // Ensure the user is an admin
-        if (token.role !== "admin") {
-            // Redirect unauthorized users to the home page
-            return NextResponse.redirect(new URL("/", req.url));
-        }
-    }
-
-    return NextResponse.next(); // Allow the request to proceed
-}
-
-// Apply middleware to specific paths
-export const config = {
-    matcher: ["/admin/:path*", "/api/admin/:path*"], // Apply to all admin routes
-};
+    // Existing middleware logic here
+  }
+  
